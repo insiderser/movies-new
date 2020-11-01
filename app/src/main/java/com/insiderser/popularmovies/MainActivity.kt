357293ad729b38a95af5c49790279f0e2d4d96e8
Oleksandr Bezushko <34242332+insiderser@users.noreply.github.com>
2020-11-01 16:50:30 +0200

    Initial commit

---
+++ app/src/main/java/com/insiderser/popularmovies/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.insiderser.popularmovies
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import com.insiderser.popularmovies.ui.PopularMoviesTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PopularMoviesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PopularMoviesTheme {
+        Greeting("Android")
+    }
+}
