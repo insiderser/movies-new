ab13c573562e6ba4351362ce5a06fd49d897e1f5
Oleksandr Bezushko <34242332+insiderser@users.noreply.github.com>
2020-11-16 18:43:08 +0200

    Make loading/error indicators span the whole RecyclerView width

--- app/src/main/java/com/insiderser/popularmovies/ui/common/FooterLoadStateViewHolder.kt
+++ app/src/main/java/com/insiderser/popularmovies/ui/common/FooterLoadStateViewHolder.kt
@@ -1,6 +1,8 @@
 package com.insiderser.popularmovies.ui.common
 
+import androidx.core.view.updateLayoutParams
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import androidx.viewbinding.ViewBinding
 import com.insiderser.popularmovies.databinding.ItemLoadStateErrorBinding
 import com.insiderser.popularmovies.databinding.ItemLoadStateLoadingBinding
@@ -8,6 +10,14 @@ import com.insiderser.popularmovies.databinding.ItemLoadStateLoadingBinding
 sealed class FooterLoadStateViewHolder(
     binding: ViewBinding
 ) : RecyclerView.ViewHolder(binding.root) {
+
+    init {
+        binding.root.updateLayoutParams {
+            if (this is StaggeredGridLayoutManager.LayoutParams) {
+                isFullSpan = true
+            }
+        }
+    }
 
     class Loading(
         binding: ItemLoadStateLoadingBinding
