fdc0b3e673e7f861a7994d738954e0eb3f8ef43a
Oleksandr Bezushko <34242332+insiderser@users.noreply.github.com>
2020-11-16 18:45:27 +0200

    Simplify movie item layout

--- app/src/main/java/com/insiderser/popularmovies/ui/list/MoviesListViewHolder.kt
+++ app/src/main/java/com/insiderser/popularmovies/ui/list/MoviesListViewHolder.kt
@@ -1,11 +1,10 @@
 package com.insiderser.popularmovies.ui.list
 
-import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import com.insiderser.popularmovies.R
 import com.insiderser.popularmovies.databinding.ItemMovieBinding
 import com.insiderser.popularmovies.model.Movie
-import timber.log.Timber
 
 class MoviesListViewHolder(
     private val binding: ItemMovieBinding,
@@ -15,27 +14,22 @@ class MoviesListViewHolder(
     private var currentMovie: Movie? = null
 
     init {
-        binding.cardView.setOnClickListener { view ->
-            currentMovie?.let { onItemClick(it, view) }
+        binding.image.setOnClickListener {
+            currentMovie?.let { onItemClick(it, binding.image) }
         }
     }
 
     fun bind(movie: Movie?) = with(binding) {
         currentMovie = movie
+        image.contentDescription = movie?.title
+
         image.load(movie?.posterPath) {
             crossfade(true)
-            listener(onError = { _, e ->
-                Timber.e(e)
-                showImage(false)
-            })
         }
-        image.contentDescription = movie?.title
-        title.text = movie?.title
-        showImage(movie?.posterPath != null)
-    }
 
-    private fun showImage(show: Boolean = true) = with(binding) {
-        image.isVisible = show
-        title.isVisible = !show
+        image.transitionName = root.context.getString(
+            R.string.transition_list_movies_item,
+            movie?.id.toString()
+        )
     }
 }
