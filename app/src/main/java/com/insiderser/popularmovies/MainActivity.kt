f6852d4a4088deb4c2b7f0e4f19e396d7ee692a7
Oleksandr Bezushko <34242332+insiderser@users.noreply.github.com>
2020-11-26 22:13:03 +0200

    Reformat code

--- app/src/main/java/com/insiderser/popularmovies/MainActivity.kt
+++ app/src/main/java/com/insiderser/popularmovies/MainActivity.kt
@@ -2,11 +2,9 @@ package com.insiderser.popularmovies
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.updateLayoutParams
 import com.insiderser.popularmovies.databinding.ActivityMainBinding
+import com.insiderser.popularmovies.util.applyStatusBarHeightToHeight
 import dagger.hilt.android.AndroidEntryPoint
-import dev.chrisbanes.insetter.Insetter
 import dev.chrisbanes.insetter.applySystemWindowInsetsToPadding
 import dev.chrisbanes.insetter.setEdgeToEdgeSystemUiFlags
 
@@ -20,12 +18,6 @@ class MainActivity : AppCompatActivity() {
 
         binding.root.setEdgeToEdgeSystemUiFlags()
         binding.root.applySystemWindowInsetsToPadding(left = true, right = true)
-        Insetter.builder()
-            .setOnApplyInsetsListener { view, insets, _ ->
-                view.updateLayoutParams {
-                    height = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top
-                }
-            }
-            .applyToView(binding.statusBarScrim)
+        binding.statusBarScrim.applyStatusBarHeightToHeight()
     }
 }
--- app/src/main/java/com/insiderser/popularmovies/util/InsetsExt.kt
+++ app/src/main/java/com/insiderser/popularmovies/util/InsetsExt.kt
@@ -1,6 +1,8 @@
 package com.insiderser.popularmovies.util
 
+import android.view.View
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import dev.chrisbanes.insetter.Insetter
 
@@ -11,6 +13,16 @@ fun SwipeRefreshLayout.applySystemWindowInsetsToProgressOffset() {
         .setOnApplyInsetsListener { _, insets, _ ->
             val statusBarHeight = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top
             setProgressViewOffset(false, progressViewStartOffset, initialOffset + statusBarHeight)
+        }
+        .applyToView(this)
+}
+
+fun View.applyStatusBarHeightToHeight() {
+    Insetter.builder()
+        .setOnApplyInsetsListener { _, insets, _ ->
+            updateLayoutParams {
+                height = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top
+            }
         }
         .applyToView(this)
 }
